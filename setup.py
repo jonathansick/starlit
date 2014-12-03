a7811005f1b73d99198699538f93bcb44e5b5393
Jonathan Sick <jonathansick@mac.com>
2014-12-02 20:45:33 -0500

    Setup package

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import re
+from setuptools import setup
+
+
+def rel_path(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+def get_version():
+    with open(rel_path(os.path.join("starlit", "__init__.py"))) as f:
+        for line in f:
+            if line.startswith("VERSION"):
+                version = re.findall(r'\"(.+?)\"', line)[0]
+                return version
+    return "0.0.0.dev"
+
+
+try:
+    long_description = open(rel_path('README.rst'), 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='paperweight',
+    version=get_version(),
+    author='Jonathan Sick',
+    author_email='jonathansick@mac.com',
+    license='BSD',
+    description='Tools for working with astro literature databases',
+    long_description=long_description,
+    py_modules=['starlit'],
+    install_requires=['pytest'],
+    url='https://github.com/jonathansick/starlit',
+    download_url='',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Environment :: Console'],
+)
