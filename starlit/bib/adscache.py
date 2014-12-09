0092bd5fe5dfd5f4b4d096b79b355d09fbb3e2a2
Jonathan Sick <jonathansick@mac.com>
2014-12-09 11:49:26 -0600

    Create adscache

---
+++ starlit/bib/adscache.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Cache ADS paper requests in MongoDB.
+
+2014-12-09 - Created by Jonathan Sick
+"""
+
+from pymongo import MongoClient
+from .base import BasePub
+
+
+class ADSCacheDB(object):
+    """Cache of publications built from requests to ADSBibDB.
+
+    The cache is built around MongoDB.
+    """
+    def __init__(self, host='localhost', port=27017):
+        super(ADSCacheDB, self).__init__()
+        client = MongoClient(host, port)
+        db = client['starlit']
+        self._c = db['cache']  # MongoDB collection
+
+    def __contains__(self, bibcode):
+        if self._c.find({"_id": bibcode}).count() > 0:
+            return True
+        else:
+            return False
+
+    def __getitem__(self, bibcode):
+        doc = self._c.find_one({"_id": bibcode})
+        return CachePub(doc)
+
+
+class CachePub(BasePub):
+    """A publication cached by ADSCacheDB (MongoDB).
+
+    Parameters
+    ----------
+    doc : object
+        PyMongo document instance.
+    """
+    def __init__(self, doc):
+        super(CachePub, self).__init__()
+        self._doc = doc
+
+    @property
+    def authors(self):
+        """Parsed list of authors; each author is a ``(Last, First)`` tuple."""
+        return self._doc['authors']
+
+    @property
+    def title(self):
+        """Title (unicode)"""
+        return self._doc['title']
+
+    @property
+    def abstract(self):
+        """Abstract text (unicode)."""
+        return self._doc['abstract']
+
+    @property
+    def bibcode(self):
+        """The ADS bibcode for this publication."""
+        return self._doc['bibcode']
+
+    @property
+    def references(self):
+        """Records of papers referenced by this publication."""
+        bibcodes = self._doc['reference_bibcodes']
+        # FIXME actually generate paper instances from these bibcodes
+        return bibcodes
+
+    @property
+    def citations(self):
+        """Records of papers referenced by this publication."""
+        bibcodes = self._doc['citation_bibcodes']
+        # FIXME actually generate paper instances from these bibcodes
+        return bibcodes
+
+    @property
+    def doi(self):
+        """DOI for paper."""
+        if 'doi' in self._doc:
+            return self._doc['doi']
+        else:
+            # FIXME should I throw an exception instead?
+            return None
+
+    @property
+    def arxiv_id(self):
+        """Arxiv identifier for article."""
+        print self._data.keys()
+        if 'arxiv_id' in self._doc:
+            return self._doc['arxiv_id']
